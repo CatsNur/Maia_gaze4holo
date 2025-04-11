1087dfda3fe3b7e5a5173b3e79cca9c98eec5625
Nora Castner <castner.nora@gmail.com>
2025-04-11 16:18:24 +0200

    make so that debugger visualizer can be grabbale and movable

--- Assets/Scripts/UI Functionality/TextUpdater.cs
+++ Assets/Scripts/UI Functionality/TextUpdater.cs
@@ -6,30 +6,37 @@ using TMPro;
 public class TextUpdater : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI targetText;
+    public int maxLines = 10;
+    private Queue<string> textLines = new Queue<string>();
     // Start is called before the first frame update
     /*void Start()
     {
         
     }*/
 
-    // Update is called once per frame
-    /*void Update()
-    {
-        if (targetText != null)
-        {
-            targetText.text = GetLiveText();
-        }
-    }*/
-
     // Replace this with whatever you're tracking
     public void GetLiveText(string incomingStream)
     {
-        if (targetText != null)
+        /*if (targetText != null)
         {
             targetText.text = incomingStream;
-        }
+        }*/
         // Example: show camera position and time
         //Vector3 camPos = Camera.main.transform.position;
         //return $"Camera Position:\nX: {camPos.x:F2} Y: {camPos.y:F2} Z: {camPos.z:F2}\nTime: {Time.time:F2}s";
+        if (targetText != null)
+        {
+            // Add the new line
+            textLines.Enqueue(incomingStream);
+
+            // If too many lines, remove the oldest
+            if (textLines.Count > maxLines)
+            {
+                textLines.Dequeue();
+            }
+
+            // Join lines into a single string
+            targetText.text = string.Join("\n", textLines);
+        }
     }
 }
