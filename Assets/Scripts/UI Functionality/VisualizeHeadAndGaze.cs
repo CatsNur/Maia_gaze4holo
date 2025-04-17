e2c4b4b2151cb8b300d4fbaaf4c16dc1ac297838
Nora Castner <castner.nora@gmail.com>
2025-04-17 14:49:57 +0200

    weird package manager

--- Assets/Scripts/UI Functionality/VisualizeHeadAndGaze.cs
+++ Assets/Scripts/UI Functionality/VisualizeHeadAndGaze.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MixedReality.Toolkit.Input;
-using UnityEditor.PackageManager;
+//using UnityEditor.PackageManager;
 
 public class VisualizeHeadAndGaze : MonoBehaviour
 {
